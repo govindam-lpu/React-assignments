06bb415d4da0ca71fa5e73413aec14802dfdb223
govindam-lpu <govindam.12003032@lpu.in>
2023-01-18 17:03:23 +0530

    Assignm 2

---
+++ index.js
@@ -0,0 +1,13 @@
+var array=[]
+var size=100
+
+for(var a=0;a<size;a++)
+{
+    array[a]=prompt("Enter the player in 0 and 1")
+}
+
+for(var i=0;i<100;i++){
+    if(array[i]===array[i+1]){
+        
+    }
+}
